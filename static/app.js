cee9491f40ade79ebb3179b1a39b19e4ea7c897f
Nicolas Simmonds <simmonds.nicolas@gmail.com>
2024-02-05 18:47:20 -0500

    Refactored JavaScript. Added comments.

--- static/app.js
+++ static/app.js
@@ -1,86 +1,107 @@
+// URL for the Flask app
 const url = "http://127.0.0.1:5000/";
+
+// Comparison form in the DOM
 const comparisonForm = document.getElementById("comparison-form");
 
-if (comparisonForm) {
-  comparisonForm.addEventListener("submit", async function (evt) {
-    evt.preventDefault();
+// Prevents the form from submitting and sends a POST request to the server. If the request is successful, the results are displayed on the page, the form is reset, and the history is updated. If the request fails, an error message is displayed.
+async function handleFormSubmit(evt) {
+  evt.preventDefault();
 
-    let asset_type_1;
-    if (document.getElementById("asset_type_1-0").checked) {
-      asset_type_1 = document.getElementById("asset_type_1-0").value;
-    } else if (document.getElementById("asset_type_1-1").checked) {
-      asset_type_1 = document.getElementById("asset_type_1-1").value;
-    }
+  // Gets the asset type and ticker value from the form. The csrf token is obtained from the hidden input field in the form.
+  const assetType1 = getAssetType(1);
+  const assetType2 = getAssetType(2);
+  const ticker1 = getTicker("ticker_1");
+  const ticker2 = getTicker("ticker_2");
+  const csrfToken = document.querySelector('input[name="csrf_token"]').value;
 
-    let asset_type_2;
-    if (document.getElementById("asset_type_2-0").checked) {
-      asset_type_2 = document.getElementById("asset_type_2-0").value;
-    } else if (document.getElementById("asset_type_2-1").checked) {
-      asset_type_2 = document.getElementById("asset_type_2-1").value;
-    }
+  try {
+    // Sends a POST request to the server with the form data.
+    const response = await axios.post(`${url}handle_comparison`, {
+      asset_type_1: assetType1,
+      ticker_1: ticker1,
+      asset_type_2: assetType2,
+      ticker_2: ticker2,
+      csrf_token: csrfToken,
+    });
+    // Displays result on the page.
+    showResults(response.data.results, ticker1, ticker2);
 
-    let ticker_1 = document.getElementById("ticker_1").value.toUpperCase();
-    let ticker_2 = document.getElementById("ticker_2").value.toUpperCase();
+    // Resets the form.
+    comparisonForm.reset();
 
-    let csrf_token = document.querySelector('input[name="csrf_token"]').value;
-
-    try {
-      const response = await axios.post(`${url}handle_comparison`, {
-        asset_type_1,
-        ticker_1,
-        asset_type_2,
-        ticker_2,
-        csrf_token,
-      });
-
-      let results = response.data.results;
-      let multiple = results["multiple"];
-      let percentage_change = results["percentage_change"];
-      console.log(multiple, percentage_change, response.data);
-
-      let newResultsLi = document.createElement("li");
-      if (percentage_change < 0) {
-        newResultsLi.innerHTML = `${ticker_2}'s Market Cap is ${percentage_change}% of ${ticker_1}'s. ${ticker_2} would have to perform a ${multiple}x to reach ${ticker_1}'s current valuation.`;
-      } else if (percentage_change >= 0) {
-        newResultsLi.innerHTML = `${ticker_2}'s Market Cap is ${percentage_change}% greater than ${ticker_1}'s. ${ticker_1} would have to perform a ${multiple}x to reach ${ticker_2}'s current valuation.`;
-      }
-      const resultsUl = document.querySelector("#results");
-      resultsUl.appendChild(newResultsLi);
-
-      // Clear the form
-      document.getElementById("ticker_1").value = "";
-      document.getElementById("ticker_2").value = "";
-      document.getElementById("asset_type_1-0").checked = false;
-      document.getElementById("asset_type_1-1").checked = false;
-      document.getElementById("asset_type_2-0").checked = false;
-      document.getElementById("asset_type_2-1").checked = false;
-
-      await getUserHistory();
-    } catch (error) {
-      console.log(error.response.data.message);
-    }
-  });
+    // Updates the history list.
+    await getUserHistory();
+  } catch (error) {
+    // Handles Errors during form submission.
+    handleError(error);
+  }
 }
 
-window.addEventListener("load", async function (evt) {
+// Gets the asset type from the form based on the asset type number.
+function getAssetType(assetTypeNumber) {
+  const assetType0 = document.getElementById(`asset_type_${assetTypeNumber}-0`);
+  const assetType1 = document.getElementById(`asset_type_${assetTypeNumber}-1`);
+
+  return assetType0.checked ? assetType0.value : assetType1.value;
+}
+
+// Gets the ticker value from the form, converts it to uppercase in order to standardize the input, and returns it.
+function getTicker(tickerFormId) {
+  return document.getElementById(tickerFormId).value.toUpperCase();
+}
+
+// Uses results from the server response and the tickers from the form. Creates a new list element, sets the innerHTML to the msg, sets the innerHTML of the UL element to empty to clear the previous results, and appends the new list element to the UL element.
+function showResults(results, ticker1, ticker2) {
+  const { multiple, percentage_change: percentChange } = results;
+  const msg =
+    percentChange < 0
+      ? `${ticker2}'s Market Cap is ${percentChange}% of ${ticker1}'s. ${ticker2} would have to perform a ${multiple}x to reach ${ticker1}'s current valuation.`
+      : `${ticker2}'s Market Cap is ${percentChange}% greater than ${ticker1}'s. ${ticker1} would have to perform a ${multiple}x to reach ${ticker2}'s current valuation.`;
+
+  const newResultsLi = document.createElement("li");
+  newResultsLi.innerHTML = msg;
+  const resultsUl = document.querySelector("#results");
+  resultsUl.innerHTML = "";
+  resultsUl.appendChild(newResultsLi);
+}
+
+// Logs the error to the console and displays an alert with the error message.
+function handleError(error) {
+  console.log(error.response.data.message);
+  alert(error.response.data.message);
+}
+
+// If the comparison form exists, add an event listener to it that calls handleFormSubmit when the form is submitted.
+if (comparisonForm) {
+  comparisonForm.addEventListener("submit", handleFormSubmit);
+}
+
+// When the window loads, get the user's history and display it on the page.
+window.addEventListener("load", async () => {
   if (document.getElementById("history")) await getUserHistory();
 });
 
+// Sends a GET request to the server to get the user's history. If the request is successful, calls updateHistory with the history data. If not, logs the error to the console and displays an alert with the error message.
 async function getUserHistory() {
   try {
     const response = await axios.get(`${url}get_user_history`);
-    let history = response.data.history;
-
-    const historyUl = document.querySelector("#history");
-    historyUl.innerHTML = "";
-
-    for (let i = 0; i < 9; i++) {
-      let newHistoryLi = document.createElement("li");
-      newHistoryLi.innerHTML = `Date: ${history[i].comparison_timestamp} | ${history[i].name_1} compared to ${history[i].name_2} | Percent Change: ${history[i].name_1} to ${history[i].name_2} is ${history[i].percent_difference}%`;
-      historyUl.appendChild(newHistoryLi);
-      console.log(history);
-    }
+    const history = response.data.history;
+    updateHistory(history);
   } catch (error) {
-    console.log(error.response.data.message);
+    handleError(error);
+  }
+}
+
+// Clears the history list, gets the most recent 10 comparisons, creates a new list element for each comparison, sets the innerHTML to the comparison data, and appends it to the history list.
+function updateHistory(history) {
+  const historyUl = document.querySelector("#history");
+  historyUl.innerHTML = "";
+  const recentTenHistory = history.slice(0, 10);
+
+  for (let comparison of recentTenHistory) {
+    const newHistoryLi = document.createElement("li");
+    newHistoryLi.innerHTML = `Date: ${comparison.comparison_timestamp} | ${comparison.name_1} compared to ${comparison.name_2} | Percent Change: ${comparison.name_1} to ${comparison.name_2} is ${comparison.percent_difference}%`;
+    historyUl.appendChild(newHistoryLi);
   }
 }
